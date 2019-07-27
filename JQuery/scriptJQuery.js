67731f34859703761932ccac5d4e66dd8dd99ed1
david1999erazo <erazo29@hotmail.com>
2019-07-26 19:59:04 -0500

    Add workshopList

---
+++ JQuery/scriptJQuery.js
@@ -0,0 +1,5 @@
+
+$("div").css("background-color", "purple");
+$("div.highlight").css("width", "200px");
+$("#third").css("border", "10px solid orange");
+$("div:first-child").css("color", "white");
